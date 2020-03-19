fa51ee1cf69da876fb8831ab4eed7ec0a973ac7f
Mark Nefedov <mark_nefedov@outlook.com>
2020-03-20 00:36:28 +0300

    I have no idea how to configure gradle for heroku.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,14 @@
 import net.dv8tion.jda.api.JDABuilder
 
 fun main(args : Array<String>) {
-    val token = if (args.isEmpty())
-    {
-        print("Bot token:")
-        (readLine() ?: return)
+    val token = when {
+        args.isNotEmpty() -> args[0]
+        System.getenv("BOT_TOKEN") != null -> System.getenv("BOT_TOKEN")
+        else -> {
+            print("Bot token:")
+            (readLine() ?: return)
+        }
     }
-    else
-        args[0]
     JDABuilder(token).apply {
         addEventListeners(RecordCommandHandler())
         build()
