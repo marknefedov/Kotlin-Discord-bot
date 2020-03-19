c13e911d7522258a6a1654928bf7afe161f13202
Mark Nefedov <mark_nefedov@outlook.com>
2020-03-19 23:38:19 +0300

    Simplified authentication.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,14 @@
 import net.dv8tion.jda.api.JDABuilder
 
 fun main(args : Array<String>) {
-    if (args.isEmpty())
-        print("Input bot secret as argument")
-
-    JDABuilder(args[0]).apply {
+    val token = if (args.isEmpty())
+    {
+        print("Bot token:")
+        (readLine() ?: return)
+    }
+    else
+        args[0]
+    JDABuilder(token).apply {
         addEventListeners(RecordCommandHandler())
         build()
     }
