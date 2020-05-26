b0f9dd132ed2bc77bb2d156874f685edb7539079
Mark Nefedov <mark_nefedov@outlook.com>
2020-05-26 14:35:38 +0300

    Split one giant function.

--- src/main/kotlin/RecordCommandHandler.kt
+++ src/main/kotlin/RecordCommandHandler.kt
@@ -1,4 +1,5 @@
 import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.exceptions.PermissionException
@@ -18,56 +19,51 @@ class RecordCommandHandler : ListenerAdapter()
         val guildIdLong = guild.idLong
         val guildMemberId = event.author.id
         val message = event.message
-        var textChannel : MessageChannel = message.textChannel
-        when {
-            !isRecording.getOrPut(guildIdLong){false} && message.contentRaw.startsWith("::record") -> {
-                if ((guild.getMemberById(guildMemberId)?.voiceState ?: return).inVoiceChannel())
-                {
-                    var scheduleTime = 300000L
-                    stopTask[guildIdLong]?.cancel()
-                    isRecording[guildIdLong] = true
-                    try {
-                        guild.audioManager.receivingHandler = recorders.getOrPut(guildIdLong) { AudioRecorder() }
-                    }catch (e : OutOfMemoryError){
-                        println(e.toString() + e.message)
-                        event.message.textChannel.sendMessage("Server is out of memory, try again later").queue()
-                        guild.audioManager.receivingHandler = null
-                        return
-                    }
-                    val recordChannel = (guild.getMemberById(guildMemberId)?.voiceState ?: return).channel
-                    try {
-                        guild.audioManager.openAudioConnection(recordChannel)
-                    }catch (e : PermissionException)
-                    {
-                        println(e.toString() + e.message)
-                        e.message?.let { event.message.textChannel.sendMessage(it).queue() }
-                        guild.audioManager.receivingHandler = null
-                        return
-                    }
-                    val splitMessage = message.contentRaw.split(" ")
-                    if (splitMessage.size > 1 && splitMessage[1].toLongOrNull() != null) {
-                        val newScheduleTime = splitMessage[1].toLong() * 1000
-                        if (newScheduleTime < recordLengthLimit)
-                            scheduleTime = newScheduleTime
-                        else
-                            event.channel.sendMessage("Requested record is longer than current limit (${toHumanTime(recordLengthLimit)}).").queue()
-                    }
-                    if (event.message.mentionedMembers.isNotEmpty())
-                    {
-                        val user = event.message.mentionedMembers.first().user
-                        user.openPrivateChannel().queue { textChannel = it }
-                    }
-                    scheduleStop(textChannel, guild, scheduleTime)
-                    val hms = toHumanTime(scheduleTime)
-                    event.channel.sendMessage("Starting a $hms long recording. Use `::stop` to stop record earlier.").queue()
-                    println("Start recording on ${guild.name} | ${recordChannel?.name}")
-                }
-            }
-            isRecording.getOrPut(guildIdLong){false} && message.contentRaw =="::stop" -> {
-                if ((guild.getMemberById(guildMemberId)?.voiceState ?: return).inVoiceChannel())
-                    stopTask[guild.idLong]?.run()
-            }
+        val textChannel : MessageChannel = message.textChannel
+        if (!(guild.getMemberById(guildMemberId)?.voiceState ?: return).inVoiceChannel()) return
+        if (!isRecording.getOrPut(guildIdLong){false} && message.contentRaw.startsWith("::record"))
+            startRecord(guildIdLong, guild, event, guildMemberId, message, textChannel)
+        else if (isRecording.getOrPut(guildIdLong){false} && message.contentRaw =="::stop")
+            stopTask[guild.idLong]?.run()
+    }
+
+    private fun startRecord(guildIdLong: Long, guild: Guild, event: GuildMessageReceivedEvent, guildMemberId: String, message: Message, textChannel: MessageChannel) {
+        var sendTextChannel = textChannel
+        var scheduleTime = 300000L
+        stopTask[guildIdLong]?.cancel()
+        isRecording[guildIdLong] = true
+        try {
+            guild.audioManager.receivingHandler = recorders.getOrPut(guildIdLong) { AudioRecorder() }
+        } catch (e: OutOfMemoryError) {
+            println(e.toString() + e.message)
+            event.message.textChannel.sendMessage("Server is out of memory, try again later").queue()
+            guild.audioManager.receivingHandler = null
+            return
         }
+        val recordChannel = (guild.getMemberById(guildMemberId)?.voiceState ?: return).channel
+        try {
+            guild.audioManager.openAudioConnection(recordChannel)
+        } catch (e: PermissionException) {
+            println(e.toString() + e.message)
+            e.message?.let { event.message.textChannel.sendMessage(it).queue() }
+            guild.audioManager.receivingHandler = null
+            return
+        }
+        val splitMessage = message.contentRaw.split(" ")
+        if (splitMessage.size > 1 && splitMessage[1].toLongOrNull() != null) {
+            val newScheduleTime = splitMessage[1].toLong() * 1000
+            if (newScheduleTime < recordLengthLimit)
+                scheduleTime = newScheduleTime
+            else
+                event.channel.sendMessage("Requested record is longer than current limit (${toHumanTime(recordLengthLimit)}).").queue()
+        }
+        if (event.message.mentionedMembers.isNotEmpty()) {
+            val user = event.message.mentionedMembers.first().user
+            user.openPrivateChannel().queue { sendTextChannel = it }
+        }
+        scheduleStop(sendTextChannel, guild, scheduleTime)
+        val hms = toHumanTime(scheduleTime)
+        event.channel.sendMessage("Starting a $hms long recording. Use `::stop` to stop record earlier.").queue()
     }
 
     private fun stopRecord(messageChannel: MessageChannel, guild : Guild)
