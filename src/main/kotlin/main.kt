00187c30b8b73e8fdcda1b427a32e9bff7da8c0e
Mark Nefedov <mark_nefedov@outlook.com>
2020-05-26 14:20:26 +0300

    removed unnecessary configuration options.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,45 +8,24 @@ var recordLengthLimit = 300000L
 lateinit var topggApi : DiscordBotListAPI
 
 fun main(args : Array<String>) {
-    val token = when {
-        args.isNotEmpty() -> args[0]
-        System.getenv("BOT_TOKEN") != null -> System.getenv("BOT_TOKEN")
-        else -> {
-            println("Bot token:")
-            (readLine() ?: return)
-        }
-    }
+    val token = System.getenv("BOT_TOKEN")
 
-    clientId = when {
-        args.size > 1 -> args[1]
-        System.getenv("CLIENT_ID") != null -> System.getenv("CLIENT_ID")
-        else -> {
-            println("Client id: ")
-            (readLine() ?: return)
-        }
-    }
+    clientId = System.getenv("CLIENT_ID")
 
     when {
         !System.getenv("RECORD_LENGTH").isNullOrEmpty() -> recordLengthLimit = System.getenv("RECORD_LENGTH").toLong()
     }
 
-    val eventListenersToEnable = mutableListOf(
-        RecordCommandHandler(),
-        InstantReplayCommandHandler(),
-        HelpCommand()
-        )
-
-    if (!System.getenv("TOPGG_TOKEN").isNullOrEmpty()) {
-        topggApi = DiscordBotListAPI.Builder()
-            .token(System.getenv("TOPGG_TOKEN"))
-            .botId(clientId)
-            .build()
-        eventListenersToEnable.add(StatsHandler())
-    }
-
     JDABuilder.createDefault(token).apply {
         setCompression(Compression.ZLIB)
-        addEventListeners(*eventListenersToEnable.toTypedArray())
+        addEventListeners(RecordCommandHandler(), InstantReplayCommandHandler(), HelpCommand())
+        if (!System.getenv("TOPGG_TOKEN").isNullOrEmpty()) {
+            topggApi = DiscordBotListAPI.Builder()
+                .token(System.getenv("TOPGG_TOKEN"))
+                .botId(clientId)
+                .build()
+            addEventListeners(StatsHandler())
+        }
         setAutoReconnect(true)
         build()
     }
